447f99c4b2ea88cc4eae04c1484b0a171ace4663
Waaped <rafaelvasquez.2015@gmail.com>
2018-08-12 18:53:59 -0700

    The goal is to set a timer based on the time of day, a bit too advanced for me, but this could do

--- src/script.js
+++ src/script.js
@@ -1,4 +1,9 @@
 function myFunction() {
     document.getElementById("raf").src = "./src/assets/images/rafael.PNG";
     document.getElementById("raf").style.opacity = "1";
+    document.getElementByClass('')
+}
+
+function nightMode(){
+   document.body.style.background = "red";
 }
