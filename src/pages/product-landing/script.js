9341e94989a2fdaff8d02b517e1d6059648dab12
Waaped <rafaelvasquez.2015@gmail.com>
2018-08-19 00:01:26 -0700

    Created function, and calling on load

--- src/pages/product-landing/script.js
+++ src/pages/product-landing/script.js
@@ -1,9 +1,15 @@
 //refactored
 
-var d = new Date()
-var time = d.getHours()
 
-if (time > 10)
-{
-alert("Hey Listen");
+
+
+function bgDate(){
+  var d = new Date()
+  var time = d.getHours()
+
+  if (time > 10)
+  {
+  alert("Hey Listen");
+  document.getElementByClass("bg").style.backgroundColor = "blue";
+  }
 }
