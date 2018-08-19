4cd18f24cc0bfb38c2120436cd59f18968fb95db
Waaped <rafaelvasquez.2015@gmail.com>
2018-08-19 05:04:25 -0700

    Time clock needs to be modded for PM, GMT seems like the easier option, but i don't know how to manipulate the string or numbers

--- src/pages/product-landing/script.js
+++ src/pages/product-landing/script.js
@@ -1,15 +1,24 @@
 //refactored
 
 function date(){
-  var d = new Date();
-  var time = d.getHours();
+  let d = new Date();
+  let time = d.getHours();
+  let time1 = d.getMinutes();
+  let time2 = d.getSeconcs();
+
+  if (time1 >10){
+    time1=0+"time1";
+  }
+  let combo = time+":"+time1;
+
+
+console.log(combo);
 
   if (time > 10){
-  alert("Night mode");
   document.body.style.backgroundColor = "brown";
-}
-else{
+  }
+else {
   document.body.style.background = "linear-gradient(#e66465, #9198e5)"
-}
+      }
 
 }
