00f4aae754aed945a0c6ed8d0d97e31d29e24169
Waaped <rafaelvasquez.2015@gmail.com>
2018-08-18 23:17:35 -0700

    Time adds a sense of urgency, responsive layout based on time of day, javascript is starting to look like a cool puzzle piece

--- src/script.js
+++ src/script.js
@@ -1,8 +1,10 @@
 function myFunction() {
     document.getElementById("raf").src = "./src/assets/images/rafael.PNG";
     document.getElementById("raf").style.opacity = "1";
-    document.getElementByClass('')
+    document.getElementByClass('');
 }
+
+
 
 function nightMode(){
    document.body.style.background = "red";
