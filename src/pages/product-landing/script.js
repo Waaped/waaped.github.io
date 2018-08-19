0a73bbefe66db9a1eaef05b3d95f9ecd7075cd8a
Waaped <rafaelvasquez.2015@gmail.com>
2018-08-19 00:39:54 -0700

    Removed CSS and added it straight to javascript as an else

--- src/pages/product-landing/script.js
+++ src/pages/product-landing/script.js
@@ -4,9 +4,12 @@ function date(){
   var d = new Date();
   var time = d.getHours();
 
-  if (time < 10)
-  {
+  if (time > 10){
   alert("Night mode");
   document.body.style.backgroundColor = "brown";
 }
+else{
+  document.body.style.background = "green";
+}
+
 }
