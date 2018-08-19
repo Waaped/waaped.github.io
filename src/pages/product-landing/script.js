07a16e2f1dd71aa545d1c86ad1acd2bc0bec7645
Waaped <rafaelvasquez.2015@gmail.com>
2018-08-18 23:48:32 -0700

    Needs work

--- src/pages/product-landing/script.js
+++ src/pages/product-landing/script.js
@@ -1,25 +1,9 @@
-//w3 schools, rest is up to me :)
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
+//refactored
+
+var d = new Date()
+var time = d.getHours()
+
+if (time > 10)
+{
+ document.body.style.background = "red";
 }
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
-
-
-
-
-var el = document.getElementById('txt');
-
-if  (el >= 18) {
-       document.getElementByClass("bg").style.backgroundColor = "black";
-   }
