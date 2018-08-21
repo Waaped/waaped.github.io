ca95c7109c995bcee72216c464db1671defa06a0
Waaped <rafaelvasquez.2015@gmail.com>
2018-08-20 22:24:45 -0700

    Go bored destroyed the last troll

--- src/pages/product-landing/script.js
+++ src/pages/product-landing/script.js
@@ -35,7 +35,7 @@ let combo = time+":"+time1+":"+time2;
 console.log(combo);
 
   if (time > 10){
-  document.body.style.backgroundColor = "brown";
+  document.body.style.background = "linear-gradient(black, #102b40)";
   }
 else {
   document.body.style.background = "linear-gradient(#e66465, #9198e5)"
