ff55f38ee73198044ed668296ab6f6763c35b351
Waaped <rafaelvasquez.2015@gmail.com>
2018-08-25 21:33:31 -0700

    Tomorrow for sure

--- src/script.js
+++ src/script.js
@@ -11,3 +11,9 @@ function nightMode(){
 }
 
   alert('Kept you waiting huh?');
+
+
+  setTimeout(hideDiv, 30000); //Instead of 10000 put your video's length, in milliseconds
+           function hideDiv() {
+               document.getElementById("vid").style.display="none";
+           }
