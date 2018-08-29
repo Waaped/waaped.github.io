0fa549d621ff01ecd992cad11f61e34d3205a930
Waaped <rafaelvasquez.2015@gmail.com>
2018-08-28 20:45:49 -0700

    w

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 function myFunction() {
     document.getElementById("raf").src = "./src/assets/images/rafael.PNG";
     document.getElementById("raf").style.opacity = "1";
-    document.getElementByClass('');
 }
 
 
@@ -13,7 +12,7 @@ function nightMode(){
   alert('Kept you waiting huh?');
 
 
-  setTimeout(hideDiv, 30000); //Instead of 10000 put your video's length, in milliseconds
+  setTimeout(hideDiv, 30000); //Instead of 10000 put your video's length, in milliseconds BASED STACK OVERLOW
            function hideDiv() {
                document.getElementById("vid").style.display="none";
            }
@@ -21,3 +20,8 @@ function nightMode(){
           function delayOpacity() {
             document.getElementById("vid").style.opacity="0.5";
           }
+
+
+  function loopacity() {
+
+  }
