93485ca56b4085bf39615e19bef929c24742a37a
Rafael Vasquez <rafael@rafaels-mbp.home>
2018-10-29 17:49:38 -0700

    Side nav

--- src/pages/product-landing/scripts/javascript.js
+++ src/pages/product-landing/scripts/javascript.js
@@ -1,18 +1,8 @@
-// function date(){
-//   let d = new Date();
-//   let time = d.getHours();
-//   let time1 = d.getMinutes();
-//   let time2 = d.getSeconds();
-//
-//   if (time1 <10){
-//     time1="0"+time1;
-//   }
-//   if (time2 <10){
-//     time2="0"+time2;
-//   }
-//   return (time+":"+time1+":"+time2);
-// }
 
-// inline is all the rage, my fix no good for pm am distinction, could fix later
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
 
-// document.getElementById("date").innerHTML = date();
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
