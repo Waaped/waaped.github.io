b5a41e499852f6869b7d5171e9c724cefeb8e971
Rafael Vasquez <rafael@rafaels-mbp.home>
2018-10-30 13:23:27 -0700

    Inertia

--- src/pages/carl-sagan/scripts/main.js
+++ src/pages/carl-sagan/scripts/main.js
@@ -6,3 +6,16 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+//
+// var myStr = 'this-is-a-test';
+// var newStr = myStr.replace(/-/g, '_');
+// 
+// //reg expression
+//
+// console.log( newStr );
+//
+// // This solution works globally,without the use of regEx
+// var mystring = "this,is,a,test"
+// var newchar = '|'
+// mystring = mystring.split(',').join(newchar);
